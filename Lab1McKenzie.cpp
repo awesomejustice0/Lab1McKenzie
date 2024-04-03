528c7a427ab6ce8d291238d6d1a8a1d051816441
awesomejustice0 <oreoalec@gmail.com>
2024-04-03 17:28:20 -0400

    Add project files.

---
+++ Lab1McKenzie.cpp
@@ -0,0 +1,22 @@
+/*
+Program: Lab1McKenzie
+Author: Alec McKenzie
+Date: 1/24/2024
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    float width;
+    float length;
+    float area;
+    length = 4.5;
+    width = 3.35;
+    area = length * width;
+    cout << "Rectangle Area = " << area << "\n";
+
+    cout << "Who's on First?\n";
+}
